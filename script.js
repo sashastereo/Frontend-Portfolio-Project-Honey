6af94845772affdb854f5c74645cc5aae3e2ea61
sashastereo <65127506+sashastereo@users.noreply.github.com>
2021-02-25 12:35:30 +0200

    fixed menu

--- script.js
+++ script.js
@@ -4,24 +4,19 @@ const flags = document.querySelector(".flags");
 const main_navigation = document.querySelector(".main-navigation");
 const link = document.querySelector("#nav");
 
-menuIcon.addEventListener("click", () => {
+const mql = window.matchMedia('(max-width: 992px)');
+
+const changeStyle = function() {
+    if (mql.matches) {
     navbar.classList.toggle("nav-change");
     navbar.classList.toggle("nav");
     flags.classList.toggle("flags-change");
     flags.classList.toggle("flags");
     main_navigation.classList.toggle("main-navigation-change");
     main_navigation.classList.toggle("main-navigation");
-   
+    }
+}
 
-});
+menuIcon.addEventListener("click", changeStyle);
 
-link.addEventListener("click", () => {
-    navbar.classList.toggle("nav-change");
-    navbar.classList.toggle("nav");
-    flags.classList.toggle("flags-change");
-    flags.classList.toggle("flags");
-    main_navigation.classList.toggle("main-navigation-change");
-    main_navigation.classList.toggle("main-navigation");
-   
-
-});
+link.addEventListener("click", changeStyle);
